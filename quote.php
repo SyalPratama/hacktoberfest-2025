f944febce4e5eb72811061c51cc07d52498dee13
Faisyal Pratama <168973863+SyalPratama@users.noreply.github.com>
2025-10-14 18:14:24 +0700

    Create quote.php

---
+++ quote.php
@@ -0,0 +1,10 @@
+<?php
+$quotes = [
+    "Hidup adalah perjalanan, bukan tujuan.",
+    "Kerja keras mengalahkan bakat ketika bakat tidak bekerja keras.",
+    "Belajar dari kesalahan adalah kunci sukses.",
+    "Jangan takut gagal, takutlah jika tidak mencoba.",
+];
+$quote = $quotes[array_rand($quotes)];
+?>
+<p>Quote Hari Ini: "<?= $quote ?>"</p>
