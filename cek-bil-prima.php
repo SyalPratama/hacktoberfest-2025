77a358f0f298029402d9ae9d0aa12842d18e63a0
Faisyal Pratama <168973863+SyalPratama@users.noreply.github.com>
2025-10-14 18:10:14 +0700

    Create cek-bil-prima.php

---
+++ cek-bil-prima.php
@@ -0,0 +1,19 @@
+<?php
+$result = '';
+if(isset($_POST['check'])){
+    $num = (int)$_POST['num'];
+    $isPrime = $num > 1;
+    for($i=2; $i <= sqrt($num); $i++){
+        if($num % $i == 0){
+            $isPrime = false;
+            break;
+        }
+    }
+    $result = $isPrime ? "$num adalah bilangan prima" : "$num bukan bilangan prima";
+}
+?>
+<form method="post">
+    <input type="number" name="num" required>
+    <button type="submit" name="check">Cek</button>
+</form>
+<?php if($result) echo $result; ?>
