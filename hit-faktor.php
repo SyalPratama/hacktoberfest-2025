8cd4b3dc268288cde5b00f3c25fe7de3d377ffc5
Faisyal Pratama <168973863+SyalPratama@users.noreply.github.com>
2025-10-14 18:11:09 +0700

    Create hit-faktor.php

---
+++ hit-faktor.php
@@ -0,0 +1,16 @@
+<?php
+$result = '';
+if(isset($_POST['calc'])){
+    $n = (int)$_POST['n'];
+    $fact = 1;
+    for($i=2; $i <= $n; $i++){
+        $fact *= $i;
+    }
+    $result = "Faktorial $n! = $fact";
+}
+?>
+<form method="post">
+    <input type="number" name="n" required>
+    <button type="submit" name="calc">Hitung</button>
+</form>
+<?php if($result) echo $result; ?>
